cf13cf98ce8342ff55d934865e5faa5c9ab85c43
Alexander Kuzmin <57102798+GameKuzya@users.noreply.github.com>
2019-11-24 21:48:19 +0500

    Add files via upload

---
+++ 1.6/WindowApp/src/Loader.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Loader {
+public static void main(String[] args)
+{
+    JFrame frame = new JFrame();
+    frame.setSize(800,600);
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    frame.setLocationRelativeTo(null);
+    frame.setTitle("Our first Application");
+    frame.setVisible(true);
+}
+}
+
