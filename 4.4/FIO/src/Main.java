138e1905594d405a784cf34165e72fb98d784963
Alexander Kuzmin <57102798+GameKuzya@users.noreply.github.com>
2019-12-19 13:28:56 +0500

    Update Main.java

--- 4.4/FIO/src/Main.java
+++ 4.4/FIO/src/Main.java
@@ -3,20 +3,64 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception{
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
         System.out.println("Введите ФИО:");
-        String fullname = reader.readLine();
+        String inputName = new BufferedReader(new InputStreamReader(System.in)).readLine().trim();
 
-        int firstIndex = fullname.indexOf(" ");
-        int secondIndex = fullname.lastIndexOf(" ");
+        if (inputName.equals("")) {
+            System.out.println("Вы ничего не ввели");
+        } else if (searchWord(inputName)) {
+            System.out.println("Вы ввесли всего одно слово");
+        } else if (searchOfOneSpace(inputName)) {
+            System.out.println("Вы ввесли всего два слова");
+        } else if (searchOfTwoSpaces(inputName)) {
 
-        String firstName = fullname.substring(0, firstIndex);
-        String secondName = fullname.substring(firstIndex,secondIndex);
-        String thirdName = fullname.substring(secondIndex);
+            String surname = inputName.substring(0, inputName.indexOf(" "));
+            String surnameUp = "Фамилия: " + surname;
 
-        System.out.println("Фамилия: " + firstName);
-        System.out.println("Имя: " + secondName);
-        System.out.println("Отчество: " + thirdName);
+            String name = inputName.substring(inputName.indexOf(" "), inputName.lastIndexOf(" "));
+            String nameUp = "Имя: " + name;
+
+            String patronymic = inputName.substring(inputName.lastIndexOf(" "));
+            String patronymicUp = "Отчество: " + patronymic;
+
+            System.out.println(surnameUp + "\n" + nameUp + "\n" + patronymicUp);
+        } else {
+            System.out.println("Некорректный ввод");
+        }
+    }
+
+
+    private static boolean searchWord(String inputName) {
+        int numberOfWords = inputName.length();
+        boolean findWord = true;
+        for (int startWord = 0; startWord < numberOfWords; startWord++) {
+            String oneNumber = inputName.substring(startWord, startWord + 1);
+            if (oneNumber.equals(" ")) {
+                findWord = false;
+                break;
+            } else {
+                findWord = true;
+            }
+        }
+        return findWord;
+    }
+
+    private static boolean searchOfOneSpace(String inputName) {
+        String firstWord = inputName.substring(0, inputName.indexOf(" "));
+        String secondWord = inputName.substring(0, inputName.lastIndexOf(" "));
+        return firstWord.equals(secondWord);
+    }
+
+    private static boolean searchOfTwoSpaces(String inputName) {
+        String firstPart = inputName.substring(0, inputName.lastIndexOf(" "));
+        boolean firstSpace = (firstPart.substring(firstPart.indexOf(" "), firstPart.lastIndexOf(" ") + 1)).equals(" ");
+        boolean secondSpace = (inputName.substring(inputName.lastIndexOf(" "), inputName.lastIndexOf(" ") + 1)).equals(" ");
+        if (firstSpace && secondSpace) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
+
